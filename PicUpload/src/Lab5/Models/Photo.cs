42fc9e985b9a2f35808f08b6503e2cca3cb261cc
Warsame Eg <warsameegal0@gmail.com>
2016-12-21 03:10:21 -0500

    initial commit

---
+++ PicUpload/src/Lab5/Models/Photo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Lab5.Models
+{
+    public class Photo
+    {
+        public int PhotoId
+        {
+            get;
+            set;
+        }
+        
+        [StringLength(2048)]
+        public string Title
+        {
+            get;
+            set;
+        }
+        [StringLength(2048)]
+        public string Url
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ PicUpload/src/Lab5/Models/PhotoDataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab5.Models
+{
+    public class PhotoDataContext : DbContext
+    {
+        public PhotoDataContext(DbContextOptions<PhotoDataContext>
+   options)
+    : base(options)
+        {
+
+        }
+
+        public DbSet<Photo> Photos { get; set; }
+
+    }
+}
